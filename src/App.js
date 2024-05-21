5e103e11a8124ec6e23563e481942503238b8b69
piyush-451 <89684494+piyush-451@users.noreply.github.com>
2024-05-22 00:12:35 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import Layout from "./components/Layout/Layout";
 import {AuthProvider} from '../src/context/authContext'
 import TokenContextProvider from '../src/context/tokenContext/tokenContextProvider'
+import {useEffect } from "react"
 
 function App() {
   // return <Layout />;
+  const loadScript = (src) => {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = () => {
+        resolve(true);
+      };
+      script.onerror = () => {
+        resolve(false);
+      };
+      document.body.appendChild(script);
+    });
+  };
+
+  useEffect(() => {
+    loadScript("https://checkout.razorpay.com/v1/checkout.js");
+  });
+
   return (
     <AuthProvider>
       <TokenContextProvider>
